f5add8f797e902aef25788a52a075678bed4b168
Barla Ankit <barla.x88@gmail.com>
2024-03-03 15:09:38 +0530

    Implement preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
+
+// handles line breaks
+marked.use({
+  breaks: true,
+  gfm: true,
+});
+
+const defaultInputText = `![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  # Hi!! \n## This markdown previewer let's you preview markdown in HTML format.
+ 
+
+  > **Use the following cheatSheet to write your own markdown text**\n
+
+  | Markdown Syntax | HTML Element   |
+  |-----------------|----------------|
+  | \`#\`             | \`<h1>\`         |
+  | \`##\`            | \`<h2>\`         |
+  | \`###\`           | \`<h3>\`         |
+  | \`####\`          | \`<h4>\`         |
+  | \`#####\`         | \`<h5>\`         |
+  | \`######\`        | \`<h6>\`         |
+  | \`*\` or \`_\`      | \`<em>\`         |
+  | \`**\` or \`__\`    | \`<strong>\`     |
+  | \`-\`             | \`<ul>\`         |
+  | \`1.\`            | \`<ol>\`         |
+  | \` \`\` \`         | \`<code>\`       |
+  | \`\`\`\` \`\`\` \`\`\`\`   | \`<pre>\`        |
+  | \`---\` or \`***\`  | \`<hr>\`         |
+  | \`[link](url)\`   | \`<a href="url">link</a>\` |
+  | \`![alt](url)\`   | \`<img src="url" alt="alt">\` |
+  | \`> text\`        | \`<blockquote>text</blockquote>\` |
+
+
+You can use backticks to add inline code - \`<div></div>\`
+
+  This is a code block-
+  \`\`\`js
+    function() {
+      console.log('Hello World');
+    }
+  \`\`\`
+  Read about [functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions) in JavaScript.
+
+Add list using using **-**
+
+- item 1
+- item 2
+
+`;
 
 function App() {
   const [activeTab, setActiveTab] = useState('editor');
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(defaultInputText);
+  const previewRef = useRef(null);
+
+  useEffect(() => {
+    previewRef.current.innerHTML = marked.parse(input);
+  }, [input]);
 
   return (
     <main className="container min-vh-100">
@@ -44,17 +99,17 @@ function App() {
       <div className="row display-area mx-2 mx-md-0 mt-4">
         <textarea
           id="editor"
+          defaultValue={input}
           onChange={(e) => {
-            setInput(e.target.value);
+            setInput(() => e.target.value);
           }}
           className={
             activeTab === 'editor' ? 'editor-window' : 'editor-window hidden'
           }
-        >
-          {input}
-        </textarea>
+        ></textarea>
         <div
           id="preview"
+          ref={previewRef}
           className={
             activeTab === 'preview' ? 'preview-window' : 'preview-window hidden'
           }
